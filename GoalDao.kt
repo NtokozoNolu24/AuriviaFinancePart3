24af8accb6311d41bd1a05d5ab7a98904ff3cf73
Thobeka Sithole <166172226+ST456076@users.noreply.github.com>
2026-06-15 17:45:25 +0200

    GoalsDao.kt

--- GoalDao.kt
+++ GoalDao.kt
@@ -1,29 +1,3 @@
-package com.example.open_sourcepart2.database.dao
+package com.example.open_sourcepart2.database
 
-
-import androidx.room.*
-import com.example.open_sourcepart2.database.entities.Goal
-
-
-@Dao
-interface GoalDao {
-
-
-    @Insert
-    suspend fun insertGoal(goal: Goal)
-
-
-    @Query("SELECT * FROM goals WHERE userId = :userId")
-    suspend fun getGoals(userId:Int): List<Goal>
-
-
-
-    @Update
-    suspend fun updateGoal(goal: Goal)
-
-
-
-    @Delete
-    suspend fun deleteGoal(goal: Goal)
-
-}
+annotation class GoalDao
